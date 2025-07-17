24f2fd37e508e8246bddea5cca4a9bff1c9c3a4f
july2025@tutorpro360.com <july2025@tutorpro360.com>
2025-07-17 22:06:00 +0000

    Updated STG_ORDERS

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,1 +1,9 @@
-select *,(ORDERSELLINGPRICE-ORDERCOSTPRICE) as ORDERPROFIT from {{ ref('raw_orders') }}
+select 
+o.ORDERID,o.ORDERDATE,o.SHIPDATE,o.SHIPMODE,o.ORDERSELLINGPRICE,o.ORDERCOSTPRICE,(o.ORDERSELLINGPRICE-o.ORDERCOSTPRICE) as ORDERPROFIT ,
+c.CUSTOMERNAME,c.SEGMENT,c.COUNTRY,
+p.CATEGORY,p.PRODUCTNAME,p.SUBCATEGORY
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.CUSTOMERID = c.CUSTOMERID
+left join {{ ref('raw_product') }} as p
+on o.PRODUCTID = p.PRODUCTID
